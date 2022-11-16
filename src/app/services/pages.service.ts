92f2ff8c930689c981f8b0652415cec429248c30
Greyson-Sawyer <59103721+Greyson-Sawyer@users.noreply.github.com>
2022-11-15 17:00:00 -0800

    5 files modified

--- src/app/services/pages.service.ts
+++ src/app/services/pages.service.ts
@@ -7,6 +7,7 @@ export interface Page {
   videoSrc: string;
   videoType: string;
   images: string[];
+  backgroundColor: string;
 }
 
 @Injectable({
@@ -22,15 +23,20 @@ export class PagesService {
         'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2FMidnight%20Noon%20-%20Banner.mov?alt=media&token=33d5c7e0-838d-454a-b0c3-0ad5616a40b0',
       videoType: 'video/mp4',
       images: [
-        'https://picsum.photos/1920/1080',
-        'https://picsum.photos/1920/1080',
-        'https://picsum.photos/1920/1080',
-        'https://picsum.photos/1920/1080',
-        'https://picsum.photos/1920/1080',
-        'https://picsum.photos/1920/1080',
-        'https://picsum.photos/1920/1080',
-        'https://picsum.photos/1920/1080',
+        // 'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.16.1_1.16.1.jpg?alt=media&token=79208df9-7a63-4f59-b388-3a22bd399a4a',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.34.1_1.34.1.jpg?alt=media&token=de197198-f4ef-4095-bc38-76ba292b32e7',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.35.1_1.35.1.jpg?alt=media&token=54a8a3f2-0e4e-441f-a0e2-70ca1b5a8f56',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.42.2_1.42.2.jpg?alt=media&token=e0acde0c-5043-4662-8d93-47473914b420',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.46.1_1.46.1.jpg?alt=media&token=717378a1-5432-4dce-a922-120df44977ef',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.56.1_1.56.1.jpg?alt=media&token=f53fc181-ee6f-4508-8bac-14e4ba71c881',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.65.2_1.65.2.jpg?alt=media&token=330bc863-1ca6-4e62-9e0e-b644d8777b3f',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.89.2_1.89.2.jpg?alt=media&token=61dc8e3e-15bb-4998-ac7d-88f9621ef929',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.108.1_1.108.1.jpg?alt=media&token=da0a6e44-fb37-4675-b676-830aa4ca61b7',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.143.1_1.143.1.jpg?alt=media&token=d62d1e2b-d2d7-48db-b019-593ab3870c75',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.149.1_1.149.1.jpg?alt=media&token=cb41b266-5210-4738-8d4e-80c735a74ba4',
+        'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/Midnight%20Noon%2F1.155.1_1.155.1.jpg?alt=media&token=abb84d61-a87f-423a-ace7-e29237985903',
       ],
+      backgroundColor: 'rgb(42,40,35)',
     },
     {
       title: 'Emma',
@@ -51,6 +57,7 @@ export class PagesService {
         'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/emma%2Femma_1.34.5.jpg?alt=media&token=22010783-4829-49ae-a3a7-938c0831eb89',
         'https://firebasestorage.googleapis.com/v0/b/greyson-color.appspot.com/o/emma%2Femma_1.34.2.jpg?alt=media&token=1c0eb3c8-b98c-42b3-8119-b54d34ffd851',
       ],
+      backgroundColor: 'rgb(67, 0, 6)',
     },
   ];
   constructor() {}
